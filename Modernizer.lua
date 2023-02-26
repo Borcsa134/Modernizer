600829ea095564eebce894797b9e7be2993584e9
Borisz Juhász <borisz.juhasz@gmail.com>
2023-02-26 15:16:13 +0100

    feat(lua): add ace3 lib

---
+++ Modernizer.lua
@@ -0,0 +1,7 @@
+
+Modernizer = LibStub("AceAddon-3.0"):NewAddon("Modernizer")
+Console = LibStub("AceConsole-3.0")
+
+function Modernizer:OnInitialize()
+    Console:Print("Initializing addon...")
+end
